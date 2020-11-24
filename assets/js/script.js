e141066483bd87366daa2616cafa7ba8f1a5de73
dayang4321 <dayang4321@users.noreply.github.com>
2020-11-24 07:11:43 +0100

    implement language dropdown base styles

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+
+$('.dropbtn').click(function () {
+    console.log('clicked')
+   $("#langDropdown").toggleClass("show");
+  })
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(e) {
+    if (!e.target.matches('.dropbtn')) {
+    var myDropdown = document.getElementById("langDropdown");
+      if (myDropdown.classList.contains('show')) {
+        myDropdown.classList.remove('show');
+      }
+    }
+  }
+
